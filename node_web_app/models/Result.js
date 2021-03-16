a16dca897d6bb4b2913a1cac89531b5145199f0b
ageofsagittarius <mehra.ritik99@gmail.com>
2021-03-16 15:14:36 +0530

    made Result models file

---
+++ node_web_app/models/Result.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+
+const ResultSchema = new mongoose.Schema({
+    googleId: {
+        type: String,
+        required: true
+    },
+    symptoms: {
+        type: Array
+    },
+    diseases: {
+        type: Array
+    },
+    predictionMain: {
+        type: Array
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+})
+
+module.exports = mongoose.model('Result', ResultSchema)
