027187b1d5767c8c190f487aa4e20f63813a320b
cosmoimai <iit2019037@iiita.ac.in>
2021-03-28 13:35:12 +0530

    created the doctor route and worked on /:gid route for user details

--- node_web_app/routes/user.js
+++ node_web_app/routes/user.js
@@ -18,14 +18,45 @@ router.get("/patient", (req, res) => {
   .catch((error) => res.json({ error: error.message }));
 });
 
+router.get("/doctor", (req, res) => {
+  let all = User.find({role : "doctor"})
+  .lean()
+  .then((users) => {
+    res.render('user', {role: "Doctors", users: users.reverse()})
+  })
+  .catch((error) => res.json({ error: error.message }));
+});
+
+
 router.get("/:gid", async (req, res) => {
-  let user = await User.findOne({ googleId: req.params.gid });
-  let allResults = await Result.find(
-    { googleId: req.params.gid },
-    (err, results) => {
-      res.send({ profile: user, results: results });
-    }
-  );
+  let user = await User.findOne({ googleId: req.params.gid })
+  .lean()
+  .then(async (user) => {
+    let allResults = await Result.find({ googleId: req.params.gid })
+    .lean()
+    .then((results) => {
+      var options = {
+        weekday: "long",
+        year: "numeric",
+        month: "long",
+        day: "numeric",
+        hour: "numeric",
+        minute: "numeric",
+        second: "numeric",
+      };  
+      for (var i = 0; i < results.length; i++) {
+        results[i] = {
+          resultId: results[i]._id,
+          maindisease: results[i].predictionMain[0],
+          mainpercentage: results[i].predictionMain[1],
+          createdAt: results[i].createdAt.toLocaleDateString("en-US", options),
+        };
+      }
+      res.render("dashboard", { profile: user, results: results.reverse() });
+    })
+    .catch((error) => res.json({ error: error.message }));
+  })
+  .catch((error) => res.json({ error: error.message }));
 });
 
 module.exports = router;
