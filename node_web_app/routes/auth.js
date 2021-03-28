7a8e105bcc73a77ba7eeef3f568d0956ce7c3a92
biTLegi0n <rajatmehra13195@gmail.com>
2021-03-28 13:24:36 +0530

    done the auth part of google auth

--- node_web_app/routes/auth.js
+++ node_web_app/routes/auth.js
@@ -2,7 +2,14 @@ const express = require("express");
 const passport = require("passport");
 const router = express.Router();
 
-router.get("/google", passport.authenticate("google", { scope: ["profile"] }));
+router.get('/google', passport.authenticate('google', {
+  scope: [
+      'https://www.googleapis.com/auth/userinfo.profile',
+      'https://www.googleapis.com/auth/userinfo.email'
+  ]
+}));
+
+// router.get("/google", passport.authenticate("google", { scope: ["profile"] }));
 
 router.get(
   "/google/callback",
