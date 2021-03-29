2f01800a20bbf4723da7facb2aa701520f13af0d
ageofsagittarius <iit2019038@iiita.ac.in>
2021-03-29 11:46:00 +0530

    added patient part when patient is verified

--- node_web_app/middleware/auth.js
+++ node_web_app/middleware/auth.js
@@ -1,35 +1,57 @@
 module.exports = {
   ensureAuth: (req, res, next) => {
     if (req.isAuthenticated()) {
+      res.auth = true
       if (req.user.role === undefined) {
         res.redirect("/role");
         return;
       }
       return next();
     } else {
+      res.guest = true
       res.redirect("/");
     }
   },
   ensureGuest: (req, res, next) => {
     if (req.isAuthenticated()) {
+      res.auth = true
       if (req.user.role === undefined) {
         res.redirect("/role");
         return;
       }
       res.redirect("/dashboard");
     } else {
+      res.guest = true
       return next();
     }
   },
   ensureRoleNotChosen: (req, res, next) => {
     if (req.isAuthenticated()) {
+      res.auth = true
       if (req.user.role !== undefined) {
         res.redirect("/");
         return;
       }
+      res.RoleNotChosen = true
       return next();
     } else {
+      res.guest = true
       res.redirect("/");
     }
-  }
+  },
+  ensurePatient: (req, res, next) => {
+    if (req.isAuthenticated()) {
+      res.auth = true
+      if (req.user.role === "patient") {
+        res.patient = true
+        return next();
+      }
+      else{
+        res.redirect("/dashboard")
+      }
+    } else {
+      res.guest = true
+      res.redirect("/");
+    }
+  },
 };
